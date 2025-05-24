8d3c00e9357c411e224865db46136a97ca2fe8d3
carecodeconnect <carecodeconnect@pm.me>
2025-05-24 11:15:46 +0100

    Add solution for guess game

---
+++ 1_guess/solution.py
@@ -0,0 +1,21 @@
+# Guess the number game
+import random
+secret_number = random.randint(1,20)
+print("I am thinking of a secret number between 1 and 20")
+
+# Ask player to guess 6 times
+for guesses_taken in range(1,7):
+    print("Take a guess")
+    guess = int(input('>'))
+
+    if guess < secret_number:
+        print("Your guess is too low")
+    elif guess > secret_number:
+        print("Your guess is too high")
+    else:
+        break # correct answer
+
+if guess == secret_number:
+    print("Good job! You got it in " + str(guesses_taken) + " guesses!")
+else:
+    print("Nope. The number was " + str(secret_number))
